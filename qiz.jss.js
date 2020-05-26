407f4fce9f9230435005c408f7186f37742f9089
ASHISHROMAN <65939422+ASHISHROMAN@users.noreply.github.com>
2020-05-26 15:43:06 +0545

    ADD

---
+++ qiz.jss.js
@@ -0,0 +1,44 @@
+function jjj(){
+    var opt="wrong"
+    document.getElementById("chec").innerHTML=opt;
+}
+function ccc(){
+    var ct="wrong";
+    document.getElementById("check").innerHTML=ct;
+
+
+}
+function mmm(){
+    var ct="Correct";
+    document.getElementById("deck").innerHTML=ct;
+
+}
+function goal(){
+    var lt="Wrong";
+    document.getElementById("n").innerHTML=lt;
+    var gt="wrong";
+    document.getElementById("m").innerHTML=gt;
+    var st="correct";
+    document.getElementById("p").innerHTML=st;
+}
+
+  function getinfo(){
+      var object=[
+          {
+              username:"ASHISH KC",
+              password:"12345"
+          },
+          {
+        username:"Ram ",
+        password:1234
+          }
+      ]
+   var username=document.getElementById("email").value;
+   var password=document.getElementById("pass").value;
+   document.getElementById("ashi").innerHTML="WElCOME TO YOUR ACCOUNT :"+username;
+  }
+
+  
+
+
+
